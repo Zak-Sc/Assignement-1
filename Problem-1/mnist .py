4cbca37f004c35b51db6518a73763bdd5f2f58aa
zak-ab <zakaria193@hotmail.com>
2019-02-17 20:50:01 -0500

    new Problem-1

---
+++ Problem-1/mnist .py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 27 20:34:17 2019
+
+We copied the code from this website
+https://cntk.ai/pythondocs/CNTK_103A_MNIST_DataLoader.html
+"""
+# Import the relevant modules to be used later
+from __future__ import print_function
+import gzip
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import shutil
+import struct
+import sys
+
+try:
+    from urllib.request import urlretrieve
+except ImportError:
+    from urllib import urlretrieve
+
+# Config matplotlib for inline plotting
+"""%matplotlib inline"""
+
+# Functions to load MNIST images and unpack into train and test set.
+# - loadData reads a image and formats it into a 28x28 long array
+# - loadLabels reads the corresponding label data, one for each image
+# - load packs the downloaded image and label data into a combined format to be read later by
+#   the CNTK text reader
+
+def loadData(src, cimg):
+    print ('Downloading ' + src)
+    gzfname, h = urlretrieve(src, './delete.me')
+    print ('Done.')
+    try:
+        with gzip.open(gzfname) as gz:
+            n = struct.unpack('I', gz.read(4))
+            # Read magic number.
+            if n[0] != 0x3080000:
+                raise Exception('Invalid file: unexpected magic number.')
+            # Read number of entries.
+            n = struct.unpack('>I', gz.read(4))[0]
+            if n != cimg:
+                raise Exception('Invalid file: expected {0} entries.'.format(cimg))
+            crow = struct.unpack('>I', gz.read(4))[0]
+            ccol = struct.unpack('>I', gz.read(4))[0]
+            if crow != 28 or ccol != 28:
+                raise Exception('Invalid file: expected 28 rows/cols per image.')
+            # Read data.
+            res = np.fromstring(gz.read(cimg * crow * ccol), dtype = np.uint8)
+    finally:
+        os.remove(gzfname)
+    return res.reshape((cimg, crow * ccol))
+
+
+def loadLabels(src, cimg):
+    print ('Downloading ' + src)
+    gzfname, h = urlretrieve(src, './delete.me')
+    print ('Done.')
+    try:
+        with gzip.open(gzfname) as gz:
+            n = struct.unpack('I', gz.read(4))
+            # Read magic number.
+            if n[0] != 0x1080000:
+                raise Exception('Invalid file: unexpected magic number.')
+            # Read number of entries.
+            n = struct.unpack('>I', gz.read(4))
+            if n[0] != cimg:
+                raise Exception('Invalid file: expected {0} rows.'.format(cimg))
+            # Read labels.
+            res = np.fromstring(gz.read(cimg), dtype = np.uint8)
+    finally:
+        os.remove(gzfname)
+    return res.reshape((cimg, 1))
+
+def try_download(dataSrc, labelsSrc, cimg):
+    data = loadData(dataSrc, cimg)
+    labels = loadLabels(labelsSrc, cimg)
+    return np.hstack((data, labels))
+
+# URLs for the train image and label data
+url_train_image = 'http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz'
+url_train_labels = 'http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz'
+num_train_samples = 60000
+
+print("Downloading train data")
+train = try_download(url_train_image, url_train_labels, num_train_samples)
+
+# URLs for the test image and label data
+url_test_image = 'http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz'
+url_test_labels = 'http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz'
+num_test_samples = 10000
+
+print("Downloading test data")
+test = try_download(url_test_image, url_test_labels, num_test_samples)
